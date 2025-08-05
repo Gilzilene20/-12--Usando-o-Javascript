29186f77ed1ac01cc5cd47f0c9c5353a4b7f3da1
Gilzilene20 <gos20@aluno.ifnmg.edu.br>
2025-08-05 19:25:16 +0000

    OK

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # -12--Usando-o-Javascript
 #12- Usando o Javascript
+
+Nome: Gilzilene Orneles de Sales
---
+++ main.js
@@ -0,0 +1,21 @@
+console.log('Olá mundo!');
+
+// Criando um novo elemento 
+let novoElemento = document.createElement('h1');
+// Alterando o conteúdo de texto do elemento
+novoElemento.innerText = 'Hello, World! English! (Inglês) Ok?!';
+// Selecionando o elemento body
+let elementoBody = document.body;
+// Colocando o novo elemento no body
+elementoBody.appendChild(novoElemento);
+
+// Deixando bonito agora
+novoElemento.style.backgroundColor = 'blue';
+novoElemento.style.color = 'yellow';
+
+// Criado por IA)
+let paragrafo = document.createElement('p');
+paragrafo.innerText = 'Este parágrafo foi criado com JavaScript usando criatividade!';
+paragrafo.style.fontStyle = 'italic';
+paragrafo.style.color = 'green';
+elementoBody.appendChild(paragrafo);
